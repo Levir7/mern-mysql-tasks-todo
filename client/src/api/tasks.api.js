f9c59756e41bd33b17d26469c9a4ef4bf6b314f9
Levir Olea <levirolear@outlook.com>
2023-12-26 16:49:39 -0600

    third commit

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 const URL = import.meta.env.VITE_BACKEND_URL
 //es importante cerrar con punto y coma al terminar el async por que si no, no muestra nada en consola
 export const getTasksRequest = async () => 
-    await axios.get(URL);
+    await axios.get(`${URL}/tasks`);
 
 // con axios enviamos datos a la api
 export const createTaskRequest = async (task) => 
-     await axios.post(URL, task);
+     await axios.post(`${URL}/tasks`, task);
 
 export const deleteTaskRequest = async (id) => 
-    await axios.delete(`${URL}/${id}`)
+    await axios.delete(`${URL}/tasks/${id}`)
 
 export const getTaskRequest = async (id) => 
-    await axios.get(`${URL}${id}`)
+    await axios.get(`${URL}/tasks/${id}`)
 
 export const updateTaskRequest = async (id, newFields) => 
-    await axios.put(`${URL}/${id}`, newFields)
+    await axios.put(`${URL}/tasks/${id}`, newFields)
 
 export const toggleTaskDoneRequest = async (id, done) => {
-    await axios.put(`${URL}/${id}`, {
+    await axios.put(`${URL}/tasks/${id}`, {
         done
     })}
