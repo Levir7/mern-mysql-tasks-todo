75689897c5cd0084363e2b6301cfb4a01754c470
Levir Olea <levirolear@outlook.com>
2023-12-26 16:20:38 -0600

    first commit

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,23 +1,24 @@
 import axios from 'axios';
+
+const URL = import.meta.env.VITE_BACKEND_URL
 //es importante cerrar con punto y coma al terminar el async por que si no, no muestra nada en consola
 export const getTasksRequest = async () => 
-    await axios.get('http://localhost:4000/tasks');
-
+    await axios.get(URL);
 
 // con axios enviamos datos a la api
 export const createTaskRequest = async (task) => 
-     await axios.post('http://localhost:4000/tasks', task);
+     await axios.post(URL, task);
 
 export const deleteTaskRequest = async (id) => 
-    await axios.delete(`http://localhost:4000/tasks/${id}`)
+    await axios.delete(`${URL}/${id}`)
 
 export const getTaskRequest = async (id) => 
-    await axios.get(`http://localhost:4000/tasks/${id}`)
+    await axios.get(`${URL}${id}`)
 
 export const updateTaskRequest = async (id, newFields) => 
-    await axios.put(`http://localhost:4000/tasks/${id}`, newFields)
+    await axios.put(`${URL}/${id}`, newFields)
 
 export const toggleTaskDoneRequest = async (id, done) => {
-    await axios.put(`http://localhost:4000/tasks/${id}`, {
+    await axios.put(`${URL}/${id}`, {
         done
     })}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-// import {fileURLToPath} from 'url'
-// import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+import {dirname, join} from 'path'
 import cors from 'cors'
 import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js';
@@ -9,15 +9,15 @@ const app = express();
 // const __dirname = dirname(fileURLToPath(import.meta.url)) 
 
 app.use(cors({
-    origin:  process.env.PORT_FRONT || 'http://localhost:5173' // le indicamos desde que servidor se pueden recibir datos
+    origin: process.env.FRONTEND_URL ||'http://localhost:5173' // le indicamos desde que servidor se pueden recibir datos
 }))
 
 app.use(express.json()); //procesar los datos del cliente como json
-// app.use(express.static(join(__dirname, './public')))
+// app.use(express.static(join(__dirname, '../client/dist')))
 
 app.use(indexRoutes)
 app.use(taskRoutes); 
 
-app.listen(process.env.PORT);
-console.log(`sever listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 4000);
+console.log(`sever listening on port ${4000}`);
 
